36f17ecd53b38be1a79bb5872637ddad3a4b53b7
Guernouille <froggy25@hotmail.fr>
2016-02-06 08:28:05 +0100

    Create display_pkm_data.cpp

---
+++ sources/display_pkm_data.cpp
@@ -0,0 +1,91 @@
+#include "mainwindow.h"
+
+void MainWindow::display_pkm_data(quint8 pkm_id) {
+    ui->lineEdit_Pokemon_Specie->setText(pkm_name[pkm_id]);
+    ui->spinBox_PokemonSpecie_Pointer->setValue(pkm_name_pointer[pkm_id]);
+
+    img_path = ":/pkm_sprites/";
+    img_path += QString::number(pkm_id);
+    ui->label_Pokemon_Sprite->setPixmap(img_path);
+
+    ui->spinBox_Base_HP->setValue(pkm_base_hp[pkm_id]);
+    ui->spinBox_Base_Attack->setValue(pkm_base_atk[pkm_id]);
+    ui->spinBox_Base_Defense->setValue(pkm_base_def[pkm_id]);
+    ui->spinBox_Base_Special->setValue(pkm_base_speed[pkm_id]);
+    ui->spinBox_Base_Speed->setValue(pkm_base_spc[pkm_id]);
+    ui->spinBox_BaseExperience->setValue(pkm_base_experience[pkm_id]);
+    ui->spinBox_CatchRate->setValue(pkm_catch_rate[pkm_id]);
+
+    ui->comboBox_Pokemon_Type_1->setCurrentIndex(pkm_type_1[pkm_id]);
+    ui->comboBox_Pokemon_Type_2->setCurrentIndex(pkm_type_2[pkm_id]);
+
+    ui->tableWidget_StartingMoves->setItem(0,0,new QTableWidgetItem(move_name[pkm_start_move_1[pkm_id]]));
+    ui->tableWidget_StartingMoves->setItem(1,0,new QTableWidgetItem(move_name[pkm_start_move_2[pkm_id]]));
+    ui->tableWidget_StartingMoves->setItem(2,0,new QTableWidgetItem(move_name[pkm_start_move_3[pkm_id]]));
+    ui->tableWidget_StartingMoves->setItem(3,0,new QTableWidgetItem(move_name[pkm_start_move_4[pkm_id]]));
+
+    ui->tableWidget_MoveLevel_RB->setItem(0,0,new QTableWidgetItem(QString::number(pkm_rb_lvl[pkm_id][0])));
+    ui->tableWidget_MoveLevel_RB->setItem(1,0,new QTableWidgetItem(QString::number(pkm_rb_lvl[pkm_id][1])));
+    ui->tableWidget_MoveLevel_RB->setItem(2,0,new QTableWidgetItem(QString::number(pkm_rb_lvl[pkm_id][2])));
+    ui->tableWidget_MoveLevel_RB->setItem(3,0,new QTableWidgetItem(QString::number(pkm_rb_lvl[pkm_id][3])));
+    ui->tableWidget_MoveLevel_RB->setItem(4,0,new QTableWidgetItem(QString::number(pkm_rb_lvl[pkm_id][4])));
+    ui->tableWidget_MoveLevel_RB->setItem(5,0,new QTableWidgetItem(QString::number(pkm_rb_lvl[pkm_id][5])));
+    ui->tableWidget_MoveLevel_RB->setItem(6,0,new QTableWidgetItem(QString::number(pkm_rb_lvl[pkm_id][6])));
+    ui->tableWidget_MoveLevel_RB->setItem(7,0,new QTableWidgetItem(QString::number(pkm_rb_lvl[pkm_id][7])));
+    ui->tableWidget_MoveLevel_RB->setItem(8,0,new QTableWidgetItem(QString::number(pkm_rb_lvl[pkm_id][8])));
+    ui->tableWidget_MoveLevel_RB->setItem(9,0,new QTableWidgetItem(QString::number(pkm_rb_lvl[pkm_id][9])));
+    ui->tableWidget_MoveLevel_RB->setItem(0,1,new QTableWidgetItem(move_name[pkm_rb_move[pkm_id][0]]));
+    ui->tableWidget_MoveLevel_RB->setItem(1,1,new QTableWidgetItem(move_name[pkm_rb_move[pkm_id][1]]));
+    ui->tableWidget_MoveLevel_RB->setItem(2,1,new QTableWidgetItem(move_name[pkm_rb_move[pkm_id][2]]));
+    ui->tableWidget_MoveLevel_RB->setItem(3,1,new QTableWidgetItem(move_name[pkm_rb_move[pkm_id][3]]));
+    ui->tableWidget_MoveLevel_RB->setItem(4,1,new QTableWidgetItem(move_name[pkm_rb_move[pkm_id][4]]));
+    ui->tableWidget_MoveLevel_RB->setItem(5,1,new QTableWidgetItem(move_name[pkm_rb_move[pkm_id][5]]));
+    ui->tableWidget_MoveLevel_RB->setItem(6,1,new QTableWidgetItem(move_name[pkm_rb_move[pkm_id][6]]));
+    ui->tableWidget_MoveLevel_RB->setItem(7,1,new QTableWidgetItem(move_name[pkm_rb_move[pkm_id][7]]));
+    ui->tableWidget_MoveLevel_RB->setItem(8,1,new QTableWidgetItem(move_name[pkm_rb_move[pkm_id][8]]));
+    ui->tableWidget_MoveLevel_RB->setItem(9,1,new QTableWidgetItem(move_name[pkm_rb_move[pkm_id][9]]));
+
+    ui->tableWidget_MoveLevel_Y->setItem(0,0,new QTableWidgetItem(QString::number(pkm_y_lvl[pkm_id][0])));
+    ui->tableWidget_MoveLevel_Y->setItem(1,0,new QTableWidgetItem(QString::number(pkm_y_lvl[pkm_id][1])));
+    ui->tableWidget_MoveLevel_Y->setItem(2,0,new QTableWidgetItem(QString::number(pkm_y_lvl[pkm_id][2])));
+    ui->tableWidget_MoveLevel_Y->setItem(3,0,new QTableWidgetItem(QString::number(pkm_y_lvl[pkm_id][3])));
+    ui->tableWidget_MoveLevel_Y->setItem(4,0,new QTableWidgetItem(QString::number(pkm_y_lvl[pkm_id][4])));
+    ui->tableWidget_MoveLevel_Y->setItem(5,0,new QTableWidgetItem(QString::number(pkm_y_lvl[pkm_id][5])));
+    ui->tableWidget_MoveLevel_Y->setItem(6,0,new QTableWidgetItem(QString::number(pkm_y_lvl[pkm_id][6])));
+    ui->tableWidget_MoveLevel_Y->setItem(7,0,new QTableWidgetItem(QString::number(pkm_y_lvl[pkm_id][7])));
+    ui->tableWidget_MoveLevel_Y->setItem(8,0,new QTableWidgetItem(QString::number(pkm_y_lvl[pkm_id][8])));
+    ui->tableWidget_MoveLevel_Y->setItem(9,0,new QTableWidgetItem(QString::number(pkm_y_lvl[pkm_id][9])));
+    ui->tableWidget_MoveLevel_Y->setItem(0,1,new QTableWidgetItem(move_name[pkm_y_move[pkm_id][0]]));
+    ui->tableWidget_MoveLevel_Y->setItem(1,1,new QTableWidgetItem(move_name[pkm_y_move[pkm_id][1]]));
+    ui->tableWidget_MoveLevel_Y->setItem(2,1,new QTableWidgetItem(move_name[pkm_y_move[pkm_id][2]]));
+    ui->tableWidget_MoveLevel_Y->setItem(3,1,new QTableWidgetItem(move_name[pkm_y_move[pkm_id][3]]));
+    ui->tableWidget_MoveLevel_Y->setItem(4,1,new QTableWidgetItem(move_name[pkm_y_move[pkm_id][4]]));
+    ui->tableWidget_MoveLevel_Y->setItem(5,1,new QTableWidgetItem(move_name[pkm_y_move[pkm_id][5]]));
+    ui->tableWidget_MoveLevel_Y->setItem(6,1,new QTableWidgetItem(move_name[pkm_y_move[pkm_id][6]]));
+    ui->tableWidget_MoveLevel_Y->setItem(7,1,new QTableWidgetItem(move_name[pkm_y_move[pkm_id][7]]));
+    ui->tableWidget_MoveLevel_Y->setItem(8,1,new QTableWidgetItem(move_name[pkm_y_move[pkm_id][8]]));
+    ui->tableWidget_MoveLevel_Y->setItem(9,1,new QTableWidgetItem(move_name[pkm_y_move[pkm_id][9]]));
+
+    tmhm_byte = 0;
+    tmhm_flag = 0;
+
+    for(short i=1;i<56;i++){
+        tmhm_flag = (i-1)%8;
+        tmhm_byte = (i-1)/8;
+
+        QCheckBox *tmhm_chkboxs = qobject_cast<QCheckBox *>(ui->tableWidget_MoveTM->cellWidget(i-1,0));
+
+        if((1<<tmhm_flag) & pkm_tmhm_flags[pkm_id][tmhm_byte]){
+            tmhm_chkboxs->setChecked(true);
+        }
+        else{
+            tmhm_chkboxs->setChecked(false);
+        }
+    }
+
+
+    ui->textEdit_PokedexEntry->setText(pokedex_entry[pkm_id]);
+    ui->spinBox_Pokedex_Pointer->setValue(pokedex_entry_pointer[pkm_id]);
+
+    ui->comboBox_GrowthRate->setCurrentIndex(pkm_growth_rate[pkm_id]);
+}
