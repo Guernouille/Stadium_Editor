c1d75c46f8639e2bdff4aa755fa9f77c30737545
Guernouille <froggy25@hotmail.fr>
2016-02-06 08:28:25 +0100

    Create display_move_data.cpp

---
+++ sources/display_move_data.cpp
@@ -0,0 +1,13 @@
+#include "mainwindow.h"
+
+void MainWindow::display_move_data(quint8 move_id) {
+    ui->lineEdit_Move_Name->setText(move_name[move_id]);
+    ui->spinBox_MoveName_Pointer->setValue(move_name_pointer[move_id]);
+    ui->spinBox_MoveID->setValue(move_iid[move_id]);
+    ui->spinBox_MoveAccuracy->setValue(move_accuracy[move_id]);
+    ui->spinBox_MovePower->setValue(move_power[move_id]);
+    ui->spinBox_MovePP->setValue(move_pp[move_id]);
+    ui->comboBox_MoveType->setCurrentIndex(move_type[move_id]);
+    ui->comboBox_MoveEffect->setCurrentIndex(move_effect[move_id]);
+    ui->textEdit_MoveDescrText->setText(move_description[move_id]);
+}
