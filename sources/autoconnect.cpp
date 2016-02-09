488fde8cc529c584ed4c57925cde8ac3c442e8fd
Guernouille <froggy25@hotmail.fr>
2016-02-09 05:28:14 +0100

    Update autoconnect.cpp

--- sources/autoconnect.cpp
+++ sources/autoconnect.cpp
@@ -1,5 +1,10 @@
 #include "mainwindow.h"
 
+void MainWindow::on_comboBox_CPU_Trainer_currentIndexChanged(int) {
+    buf8 = ui->comboBox_CPU_Trainer->currentIndex();
+    display_cpu_trainer_pkmn(buf8);
+}
+
 void MainWindow::on_comboBox_PokemonSpecies_currentIndexChanged(int) {
     buf8 = ui->comboBox_PokemonSpecies->currentIndex();
     display_pkm_data(buf8+1);
