4c1518b56632ce53e0cb8d92834182ed129b8889
Guernouille <froggy25@hotmail.fr>
2016-05-21 02:41:22 +0200

    Various fixes
    ROM offsets are corrected
    Reads Rental data at the correct address for every Cup
    Last Rental page displays the right Cup

--- sources/read_cpu_rentals.cpp
+++ sources/read_cpu_rentals.cpp
@@ -21,7 +21,9 @@ void MainWindow::read_cpu_rentals(QFile &romfile)
         // read>>cpu_rentals_arraylen[i];
     }
 
+    current_rentals_cup = 0;
     current_rentals_number = 0;
+    current_cpu_trainers_cup = 0;
     current_cpu_trainers_number = 0;
 
     std::set<int>::const_iterator set_iter(cpu_rentals_pointers.begin()), set_end(cpu_rentals_pointers.end());
@@ -34,22 +36,24 @@ void MainWindow::read_cpu_rentals(QFile &romfile)
         // Rental Pokémon
         if(total_cpu_rentals>16){
             for(short i=current_rentals_number;i<(total_cpu_rentals+current_rentals_number);i++){
-                rom_offset = 0x898004 + i*84 + *set_iter;
+                rental_cup_id[i] = current_rentals_cup;
+
+                rom_offset = 0x898004 + (i-current_rentals_number)*84 + *set_iter;
                 romfile.seek(rom_offset);
                 read>>rental_pkm_id[i];
 
-                rom_offset = 0x898008 + i*84 + *set_iter;
+                rom_offset = 0x898008 + (i-current_rentals_number)*84 + *set_iter;
                 romfile.seek(rom_offset);
                 read>>rental_pkm_level[i];
 
-                rom_offset = 0x89800D + i*84 + *set_iter;
+                rom_offset = 0x89800D + (i-current_rentals_number)*84 + *set_iter;
                 romfile.seek(rom_offset);
                 read>>rental_pkm_move_1[i];
                 read>>rental_pkm_move_2[i];
                 read>>rental_pkm_move_3[i];
                 read>>rental_pkm_move_4[i];
 
-                rom_offset = 0x898014 + i*84 + *set_iter;
+                rom_offset = 0x898014 + (i-current_rentals_number)*84 + *set_iter;
                 romfile.seek(rom_offset);
                 read>>rental_pkm_experience[i];
                 read>>rental_pkm_ev_hp[i];
@@ -63,23 +67,47 @@ void MainWindow::read_cpu_rentals(QFile &romfile)
                 read>>rental_pkm_pp_3[i];
                 read>>rental_pkm_pp_4[i];
 
-                rom_offset = 0x898034 + i*84 + *set_iter;
+                rom_offset = 0x89802A + (i-current_rentals_number)*84 + *set_iter;
                 romfile.seek(rom_offset);
-                read>>buf8;
+                read>>rental_pkm_stat_hp[i];
+                read>>rental_pkm_stat_atk[i];
+                read>>rental_pkm_stat_def[i];
+                read>>rental_pkm_stat_spc[i];
+                read>>rental_pkm_stat_speed[i];
 
                 // Rental Pokémon nickname
+                rom_offset = 0x898034 + (i-current_rentals_number)*84 + *set_iter;
+                romfile.seek(rom_offset);
+                read>>buf8;
                 while(buf8 != 0 && buf8 != 0xFF && rental_pkm_nickname[i].size()<11){
                     rental_pkm_nickname[i] += char_table[buf8];
                     rom_offset++;
                     romfile.seek(rom_offset);
                     read>>buf8;
                 }
+
+                // Rental Pokémon IVs
+                rental_pkm_iv_atk[i] = rental_pkm_ivs[i] >> 12;
+                rental_pkm_iv_def[i] = (rental_pkm_ivs[i] >> 8) & 0xF;
+                rental_pkm_iv_speed[i] = (rental_pkm_ivs[i] >> 4) & 0xF;
+                rental_pkm_iv_spc[i] = rental_pkm_ivs[i] & 0xF;
+                rental_pkm_iv_hp[i] = (rental_pkm_iv_atk[i] & 1)*8 + (rental_pkm_iv_def[i] & 1)*4 + (rental_pkm_iv_speed[i] & 1)*2 + (rental_pkm_iv_spc[i] & 1);
+
+                // Rental Pokémon PP Ups
+                rental_pkm_ppup_1[i] = rental_pkm_pp_1[i] >> 6;
+                rental_pkm_ppup_2[i] = rental_pkm_pp_2[i] >> 6;
+                rental_pkm_ppup_3[i] = rental_pkm_pp_3[i] >> 6;
+                rental_pkm_ppup_4[i] = rental_pkm_pp_4[i] >> 6;
             }
+            current_rentals_cup++;
             current_rentals_number += total_cpu_rentals;
+            rental_cup_offset[current_rentals_cup] = current_rentals_number;
         }
         // CPU Pokémon
         else{
             for(short i=current_cpu_trainers_number;i<(total_cpu_rentals+current_cpu_trainers_number);i++){
+                cpu_cup_id[i] = current_cpu_trainers_cup;
+
                 rom_offset = 0x898004 + (i-current_cpu_trainers_number)*560 + *set_iter;
                 romfile.seek(rom_offset);
                 read>>buf8;
@@ -136,11 +164,18 @@ void MainWindow::read_cpu_rentals(QFile &romfile)
                     read>>cpu_pkm_pp_3[i][j];
                     read>>cpu_pkm_pp_4[i][j];
 
+                    rom_offset = 0x898062 + (i-current_cpu_trainers_number)*560 + j*84 + *set_iter;
+                    romfile.seek(rom_offset);
+                    read>>cpu_pkm_stat_hp[i][j];
+                    read>>cpu_pkm_stat_atk[i][j];
+                    read>>cpu_pkm_stat_def[i][j];
+                    read>>cpu_pkm_stat_spc[i][j];
+                    read>>cpu_pkm_stat_speed[i][j];
+
+                    // CPU Trainer Pokémon nickname
                     rom_offset = 0x89806C + (i-current_cpu_trainers_number)*560 + j*84 + *set_iter;
                     romfile.seek(rom_offset);
                     read>>buf8;
-
-                    // CPU Trainer Pokémon nickname
                     while(buf8 != 0 && buf8 != 0xFF && cpu_pkm_nickname[i][j].size()<11){
                         cpu_pkm_nickname[i][j] += char_table[buf8];
                         rom_offset++;
@@ -162,7 +197,13 @@ void MainWindow::read_cpu_rentals(QFile &romfile)
                     cpu_pkm_ppup_4[i][j] = cpu_pkm_pp_4[i][j] >> 6;
                 }
             }
+            current_cpu_trainers_cup++;
             current_cpu_trainers_number += total_cpu_rentals;
+            cpu_cup_offset[current_cpu_trainers_cup] = current_cpu_trainers_number;
         }
+    }
+
+    if(current_rentals_number%6!=0 && (current_rentals_number+5)<1024){
+        rental_cup_id[current_rentals_number+4] = current_rentals_cup-1;
     }
 }
