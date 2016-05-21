d3e0299f20b2f45b7749d96783c8e0abdffb8d05
Guernouille <froggy25@hotmail.fr>
2016-05-21 02:38:28 +0200

    Properly jump to correct Rental page

--- sources/autoconn_display_rental_pkm.cpp
+++ sources/autoconn_display_rental_pkm.cpp
@@ -1,15 +1,21 @@
 #include "mainwindow.h"
 
 void MainWindow::on_comboBox_Rental_Cup_currentIndexChanged(int) {
-    //buf8 = ui->comboBox_Rental_Cup->currentIndex();
-    //if(ui->comboBox_Rental_Page->currentIndex()<rental_cup_offset[buf8]||ui->comboBox_Rental_Page->currentIndex()>=rental_cup_offset[buf8+1]){
-    //    ui->comboBox_Rental_Page->setCurrentIndex(rental_cup_offset[buf8]);
-    //}
+    if(not_in_init){
+        buf8 = ui->comboBox_Rental_Cup->currentIndex();
+        if(ui->comboBox_Rental_Page->currentIndex()<(rental_cup_offset[buf8]/6) || ui->comboBox_Rental_Page->currentIndex()>=(rental_cup_offset[buf8+1]/6)) {
+            ui->comboBox_Rental_Page->setCurrentIndex(rental_cup_offset[buf8]/6);
+        }
+    }
 }
 
 void MainWindow::on_comboBox_Rental_Page_currentIndexChanged(int) {
-    buf16 = ui->comboBox_Rental_Page->currentIndex();
-    buf16 *= 6;
-    display_rental_pkmn(buf16);
-    ui->comboBox_Rental_Cup->setCurrentIndex(rental_cup_id[buf16]);
+    if(not_in_init){
+        buf16 = ui->comboBox_Rental_Page->currentIndex();
+        buf16 *= 6;
+        not_in_init=false;
+        ui->comboBox_Rental_Cup->setCurrentIndex(rental_cup_id[buf16+5]);
+        not_in_init=true;
+        display_rental_pkmn(buf16);
+    }
 }
