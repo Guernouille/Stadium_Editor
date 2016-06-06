f93e108d577e0d95faeb56f74601219d46f4fe07
Guernouille <froggy25@hotmail.fr>
2016-06-06 20:03:17 +0200

    Read Type chart

--- sources/read_type_data.cpp
+++ sources/read_type_data.cpp
@@ -1,5 +1,35 @@
 #include "mainwindow.h"
 
+void MainWindow::read_type_chart(QFile &romfile)
+{
+    QDataStream read(&romfile);
+    romfile.seek(0x3755B0);
+
+    for(short i=0;i<82;i++){
+        read>>buf8;
+        if(buf8 > 0x1A) this->romtype=INVALID;
+        type_chart[i].setType1(buf8);
+        read>>buf8;
+        if(buf8 > 0x1A) this->romtype=INVALID;
+        type_chart[i].setType2(buf8);
+        read>>buf8;
+        if(buf8 == 0xFF) this->romtype=INVALID;
+        type_chart[i].setMultiplier(buf8);
+    }
+    read>>buf8;
+    if(buf8 != 0xFF) this->romtype=INVALID;
+
+    if(this->romtype==INVALID){
+        ui->label_rom_debug->setText("Corrupted ROM");
+
+        QMessageBox messageBox;
+        messageBox.setText("Type chart is corrupted.");
+        messageBox.setWindowTitle("Type chart");
+        messageBox.exec();
+    }
+}
+
+
 void MainWindow::read_type_names(QFile &romfile)
 {
     // ***** Retrieve number of type names *****
