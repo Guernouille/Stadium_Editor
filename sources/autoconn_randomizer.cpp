a10fe3bad60f463a72fb4ba8088e9f93e2ef17ba
Guernouille <froggy25@hotmail.fr>
2016-06-07 04:17:35 +0200

    Randomize types, reordering stuff
    And changed to a single Randomize button
    Need to fix the autoconnect on checkBox_CPUTeams and checkBox_RentalPkmn

--- sources/autoconn_randomizer.cpp
+++ sources/autoconn_randomizer.cpp
@@ -1,63 +1,5 @@
 #include "mainwindow.h"
 
-// Gym Leader Castle Levels
-void MainWindow::on_checkBox_Randomizer_CPU_GLPkmn_stateChanged(int state)
-{
-    if(not_in_init){
-        not_in_init = false;
-        if(state == Qt::Checked){
-            if(glc_level_min < 10){
-                glc_level_min = 10;
-                ui->spinBox_Randomizer_GLCLevelRange_1->setValue(glc_level_min);
-                if(glc_level_max < glc_level_min){
-                    glc_level_max = glc_level_min;
-                    ui->spinBox_Randomizer_GLCLevelRange_2->setValue(glc_level_max);
-                }
-            }
-            if(glc_level_max > 78){
-                glc_level_max = 78;
-                ui->spinBox_Randomizer_GLCLevelRange_2->setValue(glc_level_max);
-                if(glc_level_min > glc_level_max){
-                    glc_level_min = glc_level_max;
-                    ui->spinBox_Randomizer_GLCLevelRange_1->setValue(glc_level_min);
-                }
-            }
-        }
-        not_in_init = true;
-    }
-}
-
-void MainWindow::on_spinBox_Randomizer_GLCLevelRange_1_valueChanged(int)
-{
-    if(not_in_init){
-        not_in_init = false;
-        glc_level_min = ui->spinBox_Randomizer_GLCLevelRange_1->value();
-        if(glc_level_min > glc_level_max){
-            glc_level_max = glc_level_min;
-            ui->spinBox_Randomizer_GLCLevelRange_2->setValue(glc_level_max);
-        }
-        if(glc_level_max > 78){
-            ui->checkBox_Randomizer_CPU_GLPkmn->setChecked(false);
-        }
-        not_in_init = true;
-    }
-}
-void MainWindow::on_spinBox_Randomizer_GLCLevelRange_2_valueChanged(int)
-{
-    if(not_in_init){
-        not_in_init = false;
-        glc_level_max = ui->spinBox_Randomizer_GLCLevelRange_2->value();
-        if(glc_level_max < glc_level_min){
-            glc_level_min = glc_level_max;
-            ui->spinBox_Randomizer_GLCLevelRange_1->setValue(glc_level_min);
-        }
-        if(glc_level_max > 78){
-            ui->checkBox_Randomizer_CPU_GLPkmn->setChecked(false);
-        }
-        not_in_init = true;
-    }
-}
-
 // Max PPs (Rental)
 void MainWindow::on_checkBox_Randomizer_Rental_MaxPPUps_stateChanged(int state)
 {
@@ -75,30 +17,102 @@ void MainWindow::on_checkBox_Randomizer_Rental_NoPPUps_stateChanged(int state)
 }
 
 
-// Randomize (CPU)
-void MainWindow::on_pushButton_Randomize_CPU_Teams_pressed()
+// Randomize
+void MainWindow::on_pushButton_Randomize_pressed()
 {
     std::seed_seq seeds{prng_seed[0], prng_seed[1], prng_seed[2], prng_seed[3], prng_seed[4], prng_seed[5]};
     std::mt19937 mt_rand(seeds);
     mt_rand.discard(700000);
 
-    if(ui->checkBox_Randomize_CPU_Levels->isChecked()){
-        randomize_cpu_level(mt_rand);
+    // CPU Teams
+    if(ui->checkBox_CPUTeams->isChecked()){
+        if(ui->checkBox_Randomize_CPU_Levels->isChecked()){
+            randomize_cpu_level(mt_rand);
+        }
+
+        if(ui->checkBox_Randomize_CPU_Pkmn->isChecked()){
+            randomize_cpu_init_pkmn();
+            randomize_cpu_pkmn(mt_rand);
+        }
+
+        if(ui->checkBox_Randomize_CPU_Moves->isChecked()){
+            randomize_cpu_moves(mt_rand);
+        }
+
+        if(ui->checkBox_Randomizer_CPU_Names->isChecked()){
+            randomize_cpu_nicknames(mt_rand);
+        }
+
+        // Update display
+        if(not_in_init){
+            display_cpu_trainer_pkmn(ui->comboBox_CPU_Trainer->currentIndex());
+        }
     }
 
-    if(ui->checkBox_Randomize_CPU_Pkmn->isChecked()){
-        randomize_cpu_init_pkmn();
-        randomize_cpu_pkmn(mt_rand);
+
+    // Rental
+    if(ui->checkBox_RentalPkmn->isChecked()){
+        if(ui->checkBox_Randomize_Rental_Levels->isChecked()){
+            randomize_rental_level(mt_rand);
+        }
+
+        if(ui->checkBox_Randomize_Rental_Pkmn->isChecked()){
+            randomize_rental_init_pkmn();
+            randomize_rental_pkmn(mt_rand);
+        }
+
+        if(ui->checkBox_Randomize_Rental_Moves->isChecked()){
+            randomize_rental_moves(mt_rand);
+        }
+
+        // Update display
+        if(not_in_init){
+            display_rental_pkmn(ui->comboBox_Rental_Page->currentIndex());
+        }
     }
 
-    if(ui->checkBox_Randomize_CPU_Moves->isChecked()){
-        randomize_cpu_moves(mt_rand);
+
+    // Type chart
+    if(ui->checkBox_Randomize_TypeChart->isChecked()){
+        randomize_type_chart(mt_rand);
+    }
+    else{
+        if(ui->checkBox_Ghost_vs_Psychic->isChecked()){
+            type_chart[75].setType1(0x8);
+            type_chart[75].setType2(0x1A);
+            type_chart[75].setMultiplier(20);
+        }
+        else{
+            type_chart[75].setType1(0x8);
+            type_chart[75].setType2(0x1A);
+            type_chart[75].setMultiplier(0);
+        }
+
+        if(ui->checkBox_Ice_vs_Fire->isChecked()){
+            type_chart[81].setType1(0x19);
+            type_chart[81].setType2(0x14);
+            type_chart[81].setMultiplier(5);
+        }
+        else{
+            type_chart[81].setType1(0x1A);
+            type_chart[81].setType2(0x1A);
+            type_chart[81].setMultiplier(20);
+        }
+
+        if(ui->checkBox_Bug_vs_Poison->isChecked()){
+            type_chart[67].setType1(0x7);
+            type_chart[67].setType2(0x3);
+            type_chart[67].setMultiplier(5);
+        }
+        else{
+            type_chart[67].setType1(0x7);
+            type_chart[67].setType2(0x3);
+            type_chart[67].setMultiplier(20);
+        }
     }
 
-    if(ui->checkBox_Randomize_CPU_Sprites->isChecked()){
-        randomize_cpu_nicknames(mt_rand);
-    }
 
+    // New seeds
     if(ui->checkBox_FixedSeeds->isChecked()==false){
         prng_seed[0] = mt_rand();
         prng_seed[1] = mt_rand();
@@ -108,47 +122,30 @@ void MainWindow::on_pushButton_Randomize_CPU_Teams_pressed()
         prng_seed[5] = mt_rand();
     }
 
-    // Update display
-    if(not_in_init){
-        display_cpu_trainer_pkmn(ui->comboBox_CPU_Trainer->currentIndex());
-    }
+
 }
-
-
-// Randomize (Rental)
-void MainWindow::on_pushButton_Randomize_Rental_Pkmn_pressed()
+void MainWindow::on_checkBox_CPUTeams_stateChanged(int state)
 {
-    std::seed_seq seeds{prng_seed[0], prng_seed[1], prng_seed[2], prng_seed[3], prng_seed[4], prng_seed[5]};
-    std::mt19937 mt_rand(seeds);
-
-    if(ui->checkBox_Randomize_Rental_Levels->isChecked()){
-        randomize_rental_level(mt_rand);
+    // TO DO: debug this
+    if(state == Qt::Checked){
+        ui->groupBox_Randomizer_CPU->setEnabled(true);
     }
-
-    if(ui->checkBox_Randomize_Rental_Pkmn->isChecked()){
-        randomize_rental_init_pkmn();
-        randomize_rental_pkmn(mt_rand);
+    else{
+        ui->groupBox_Randomizer_CPU->setEnabled(false);
     }
-
-    if(ui->checkBox_Randomize_Rental_Moves->isChecked()){
-        randomize_rental_moves(mt_rand);
-    }
-
-    if(ui->checkBox_FixedSeeds->isChecked()==false){
-        prng_seed[0] = mt_rand();
-        prng_seed[1] = mt_rand();
-        prng_seed[2] = mt_rand();
-        prng_seed[3] = mt_rand();
-        prng_seed[4] = mt_rand();
-        prng_seed[5] = mt_rand();
-    }
-
-    // Update display
-    if(not_in_init){
-        display_rental_pkmn(ui->comboBox_Rental_Page->currentIndex());
-    }
+    ui->checkBox_CPUTeams->setEnabled(true);
 }
-
+void MainWindow::on_checkBox_RentalPkmn_stateChanged(int state)
+{
+    // TO DO: debug this
+    if(state == Qt::Checked){
+        ui->groupBox_Randomizer_Rental->setEnabled(true);
+    }
+    else{
+        ui->groupBox_Randomizer_Rental->setEnabled(false);
+    }
+    ui->checkBox_RentalPkmn->setEnabled(true);
+}
 
 // Full Metronome (CPU)
 void MainWindow::on_pushButton_CPU_Metronome_pressed()
@@ -385,3 +382,91 @@ void MainWindow::on_pushButton_Maximize_Rental_EVsIVs_pressed()
     ui->spinBox_Rental_IV_Speed_5->setValue(0xF);
     ui->spinBox_Rental_IV_Speed_6->setValue(0xF);
 }
+
+
+// Gym Leader Castle
+void MainWindow::on_checkBox_Randomizer_CPU_GLPkmn_stateChanged(int state)
+{
+    if(not_in_init){
+        not_in_init = false;
+        if(state == Qt::Checked){
+            if(glc_level_min < 10){
+                glc_level_min = 10;
+                ui->spinBox_Randomizer_GLCLevelRange_1->setValue(glc_level_min);
+                if(glc_level_max < glc_level_min){
+                    glc_level_max = glc_level_min;
+                    ui->spinBox_Randomizer_GLCLevelRange_2->setValue(glc_level_max);
+                }
+            }
+            if(glc_level_max > 78){
+                glc_level_max = 78;
+                ui->spinBox_Randomizer_GLCLevelRange_2->setValue(glc_level_max);
+                if(glc_level_min > glc_level_max){
+                    glc_level_min = glc_level_max;
+                    ui->spinBox_Randomizer_GLCLevelRange_1->setValue(glc_level_min);
+                }
+            }
+        }
+        not_in_init = true;
+    }
+}
+void MainWindow::on_spinBox_Randomizer_GLCLevelRange_1_valueChanged(int)
+{
+    if(not_in_init){
+        not_in_init = false;
+        glc_level_min = ui->spinBox_Randomizer_GLCLevelRange_1->value();
+        if(glc_level_min > glc_level_max){
+            glc_level_max = glc_level_min;
+            ui->spinBox_Randomizer_GLCLevelRange_2->setValue(glc_level_max);
+        }
+        if(glc_level_max > 78){
+            ui->checkBox_Randomizer_CPU_GLPkmn->setChecked(false);
+        }
+        not_in_init = true;
+    }
+}
+void MainWindow::on_spinBox_Randomizer_GLCLevelRange_2_valueChanged(int)
+{
+    if(not_in_init){
+        not_in_init = false;
+        glc_level_max = ui->spinBox_Randomizer_GLCLevelRange_2->value();
+        if(glc_level_max < glc_level_min){
+            glc_level_min = glc_level_max;
+            ui->spinBox_Randomizer_GLCLevelRange_1->setValue(glc_level_min);
+        }
+        if(glc_level_max > 78){
+            ui->checkBox_Randomizer_CPU_GLPkmn->setChecked(false);
+        }
+        not_in_init = true;
+    }
+}
+
+
+// Type chart
+void MainWindow::on_checkBox_Ghost_vs_Psychic_stateChanged(int state)
+{
+    if(state == Qt::Checked){
+        ui->checkBox_Randomize_TypeChart->setChecked(false);
+    }
+}
+void MainWindow::on_checkBox_Ice_vs_Fire_stateChanged(int state)
+{
+    if(state == Qt::Checked){
+        ui->checkBox_Randomize_TypeChart->setChecked(false);
+    }
+}
+void MainWindow::on_checkBox_Bug_vs_Poison_stateChanged(int state)
+{
+    if(state == Qt::Checked){
+        ui->checkBox_Randomize_TypeChart->setChecked(false);
+    }
+}
+void MainWindow::on_checkBox_Randomize_TypeChart_stateChanged(int state)
+{
+    if(state == Qt::Checked){
+        ui->checkBox_Ghost_vs_Psychic->setChecked(false);
+        ui->checkBox_Ice_vs_Fire->setChecked(false);
+        ui->checkBox_Bug_vs_Poison->setChecked(false);
+    }
+}
+
