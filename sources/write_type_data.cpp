84c0563894c3491fb12b849bdc34829954660837
Guernouille <froggy25@hotmail.fr>
2016-06-07 04:24:26 +0200

    Create write_type_data.cpp

---
+++ sources/write_type_data.cpp
@@ -0,0 +1,15 @@
+#include "mainwindow.h"
+
+void MainWindow::write_type_chart(QFile &romfile)
+{
+    QDataStream write(&romfile);
+
+    rom_offset = 0x3755B0;
+    romfile.seek(rom_offset);
+
+    for(short i=0;i<82;i++){
+        write<<type_chart[i].getType1();
+        write<<type_chart[i].getType2();
+        write<<type_chart[i].getMultiplier();
+    }
+}
