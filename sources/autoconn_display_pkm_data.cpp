3ff52dca7315e972f06ef9ffe841bcd834478dd6
Guernouille <froggy25@hotmail.fr>
2016-06-20 21:04:21 +0200

    Release 0.1

--- sources/autoconn_display_pkm_data.cpp
+++ sources/autoconn_display_pkm_data.cpp
@@ -3,4 +3,5 @@
 void MainWindow::on_comboBox_PokemonSpecies_currentIndexChanged(int) {
     buf8 = ui->comboBox_PokemonSpecies->currentIndex();
     display_pkm_data(buf8+1);
+    ui->label_Pokedex_Number_value->setText(QString::number(buf8+1));
 }
