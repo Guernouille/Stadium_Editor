e0eeb1b90bb36ee81bc9f513be6336fefdec6ec2
Guernouille <froggy25@hotmail.fr>
2016-06-20 21:06:44 +0200

    Update autoconn_edit_move_data.cpp

--- sources/autoconn_edit_move_data.cpp
+++ sources/autoconn_edit_move_data.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 
 void MainWindow::on_spinBox_HighCH_multiplier_valueChanged(int) {
-    move_high_ch_multiplier = ui->spinBox_HighCH_multiplier->value();
+    if(not_in_init){
+        move_high_ch_multiplier = ui->spinBox_HighCH_multiplier->value();
+    }
 }
