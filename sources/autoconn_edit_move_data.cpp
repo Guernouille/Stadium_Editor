6195c47dcaa508a4e4c4619c20db24d66cc69681
Guernouille <froggy25@hotmail.fr>
2016-10-24 06:25:41 +0200

    typo

--- sources/autoconn_edit_move_data.cpp
+++ sources/autoconn_edit_move_data.cpp
@@ -3,35 +3,35 @@
 void MainWindow::on_comboBox_MoveEffect_currentIndexChanged(int) {
     if(not_in_init){
         buf8 = ui->comboBox_MovesList->currentIndex();
-        move_effect[buf8] = ui->comboBox_MoveEffect->value();
+        move_effect[buf8] = ui->comboBox_MoveEffect->currentIndex();
     }
 }
 void MainWindow::on_comboBox_MoveType_currentIndexChanged(int) {
     if(not_in_init){
         buf8 = ui->comboBox_MovesList->currentIndex();
-        move_type[buf8] = ui->comboBox_MoveType->value();
+        move_type[buf8] = ui->comboBox_MoveType->currentIndex();
     }
 }
 
 
 void MainWindow::on_comboBox_HighCH_MovesList_1_currentIndexChanged(int) {
     if(not_in_init){
-        move_high_ch[0] = ui->comboBox_HighCH_MovesList_1->value();
+        move_high_ch[0] = ui->comboBox_HighCH_MovesList_1->currentIndex();
     }
 }
 void MainWindow::on_comboBox_HighCH_MovesList_2_currentIndexChanged(int) {
     if(not_in_init){
-        move_high_ch[1] = ui->comboBox_HighCH_MovesList_2->value();
+        move_high_ch[1] = ui->comboBox_HighCH_MovesList_2->currentIndex();
     }
 }
 void MainWindow::on_comboBox_HighCH_MovesList_3_currentIndexChanged(int) {
     if(not_in_init){
-        move_high_ch[2] = ui->comboBox_HighCH_MovesList_3->value();
+        move_high_ch[2] = ui->comboBox_HighCH_MovesList_3->currentIndex();
     }
 }
 void MainWindow::on_comboBox_HighCH_MovesList_4_currentIndexChanged(int) {
     if(not_in_init){
-        move_high_ch[3] = ui->comboBox_HighCH_MovesList_4->value();
+        move_high_ch[3] = ui->comboBox_HighCH_MovesList_4->currentIndex();
     }
 }
 
@@ -45,16 +45,19 @@ void MainWindow::on_spinBox_HighCH_multiplier_valueChanged(int) {
 
 void MainWindow::on_spinBox_MovePower_valueChanged(int) {
     if(not_in_init){
-        move_power = ui->spinBox_MovePower->value();
+        buf8 = ui->comboBox_MovesList->currentIndex();
+        move_power[buf8] = ui->spinBox_MovePower->value();
     }
 }
 void MainWindow::on_spinBox_MoveAccuracy_valueChanged(int) {
     if(not_in_init){
-        move_accuracy = ui->spinBox_MoveAccuracy->value();
+        buf8 = ui->comboBox_MovesList->currentIndex();
+        move_accuracy[buf8] = ui->spinBox_MoveAccuracy->value();
     }
 }
 void MainWindow::on_spinBox_MovePP_valueChanged(int) {
     if(not_in_init){
-        move_pp = ui->spinBox_MovePP->value();
+        buf8 = ui->comboBox_MovesList->currentIndex();
+        move_pp[buf8] = ui->spinBox_MovePP->value();
     }
 }
