44ab8a595eeae886a98cd60bd309cdfb49529c09
Guernouille <froggy25@hotmail.fr>
2016-10-24 06:35:49 +0200

    No change when displaying a move data

--- sources/display_move_data.cpp
+++ sources/display_move_data.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 
 void MainWindow::display_move_data(quint8 move_id) {
+    not_in_init=false;
     ui->lineEdit_Move_Name->setText(move_name[move_id]);
     ui->spinBox_MoveName_Pointer->setValue(move_name_pointer[move_id]);
     ui->spinBox_MoveID->setValue(move_iid[move_id]);
@@ -10,4 +11,5 @@ void MainWindow::display_move_data(quint8 move_id) {
     ui->comboBox_MoveType->setCurrentIndex(move_type[move_id]);
     ui->comboBox_MoveEffect->setCurrentIndex(move_effect[move_id]);
     ui->textEdit_MoveDescrText->setText(move_description[move_id]);
+    not_in_init=true;
 }
