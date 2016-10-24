3c660a5c49b6607124dacae88e290febeaaebdd1
Guernouille <froggy25@hotmail.fr>
2016-10-24 07:55:24 +0200

    Save move data

--- sources/autoconn_save.cpp
+++ sources/autoconn_save.cpp
@@ -4,6 +4,7 @@ void MainWindow::on_actionSave_triggered(){
     QFile romfile(filename);
     romfile.open(QIODevice::ReadWrite);
 
+    write_move_data(romfile);
     write_pkm_data(romfile);
     write_cpu_rentals(romfile);
     write_type_chart(romfile);
