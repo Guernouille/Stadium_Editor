6cda8b729049ae9a4c8ac934715683b48f3f87ce
Guernouille <froggy25@hotmail.fr>
2016-10-24 07:51:55 +0200

    oversight

--- sources/autoconn_display_move_data.cpp
+++ sources/autoconn_display_move_data.cpp
@@ -1,6 +1,10 @@
 #include "mainwindow.h"
 
 void MainWindow::on_comboBox_MovesList_currentIndexChanged(int) {
-    buf8 = ui->comboBox_MovesList->currentIndex();
-    display_move_data(buf8+1);
+    if(not_in_init){
+        not_in_init=false;
+        buf8 = ui->comboBox_MovesList->currentIndex();
+        display_move_data(buf8+1);
+        not_in_init=true;
+    }
 }
