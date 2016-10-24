de1ee44dcd9b459c3977fcb9c6f77ea6a2ba2823
Guernouille <froggy25@hotmail.fr>
2016-10-24 07:52:21 +0200

    oversight

--- sources/autoconn_display_pkm_data.cpp
+++ sources/autoconn_display_pkm_data.cpp
@@ -1,7 +1,11 @@
 #include "mainwindow.h"
 
 void MainWindow::on_comboBox_PokemonSpecies_currentIndexChanged(int) {
-    buf8 = ui->comboBox_PokemonSpecies->currentIndex();
-    display_pkm_data(buf8+1);
-    ui->label_Pokedex_Number_value->setText(QString::number(buf8+1));
+    if(not_in_init){
+        not_in_init=false;
+        buf8 = ui->comboBox_PokemonSpecies->currentIndex();
+        display_pkm_data(buf8+1);
+        ui->label_Pokedex_Number_value->setText(QString::number(buf8+1));
+        not_in_init=true;
+    }
 }
