410bb33a2edb29cdcb9c235a4b1e8db71e913087
Guernouille <froggy25@hotmail.fr>
2016-10-24 07:51:25 +0200

    oversight

--- sources/autoconn_edit_pkm_data.cpp
+++ sources/autoconn_edit_pkm_data.cpp
@@ -3,7 +3,7 @@
 void MainWindow::on_comboBox_GrowthRate_currentIndexChanged(int) {
     if(not_in_init){
         buf8 = ui->comboBox_PokemonSpecies->currentIndex();
-        pkm_growth_rate[buf8] = ui->comboBox_GrowthRate->currentIndex();
+        pkm_growth_rate[buf8+1] = ui->comboBox_GrowthRate->currentIndex();
     }
 }
 
@@ -11,13 +11,13 @@ void MainWindow::on_comboBox_GrowthRate_currentIndexChanged(int) {
 void MainWindow::on_comboBox_Pokemon_Type_1_currentIndexChanged(int) {
     if(not_in_init){
         buf8 = ui->comboBox_PokemonSpecies->currentIndex();
-        pkm_type_1[buf8] = ui->comboBox_Pokemon_Type_1->currentIndex();
+        pkm_type_1[buf8+1] = ui->comboBox_Pokemon_Type_1->currentIndex();
     }
 }
 void MainWindow::on_comboBox_Pokemon_Type_2_currentIndexChanged(int) {
     if(not_in_init){
         buf8 = ui->comboBox_PokemonSpecies->currentIndex();
-        pkm_type_2[buf8] = ui->comboBox_Pokemon_Type_2->currentIndex();
+        pkm_type_2[buf8+1] = ui->comboBox_Pokemon_Type_2->currentIndex();
     }
 }
 
@@ -25,13 +25,13 @@ void MainWindow::on_comboBox_Pokemon_Type_2_currentIndexChanged(int) {
 void MainWindow::on_spinBox_BaseExperience_valueChanged(int) {
     if(not_in_init){
         buf8 = ui->comboBox_PokemonSpecies->currentIndex();
-        pkm_base_experience[buf8] = ui->spinBox_BaseExperience->value();
+        pkm_base_experience[buf8+1] = ui->spinBox_BaseExperience->value();
     }
 }
 void MainWindow::on_spinBox_CatchRate_valueChanged(int) {
     if(not_in_init){
         buf8 = ui->comboBox_PokemonSpecies->currentIndex();
-        pkm_catch_rate[buf8] = ui->spinBox_CatchRate->value();
+        pkm_catch_rate[buf8+1] = ui->spinBox_CatchRate->value();
     }
 }
 
@@ -39,30 +39,30 @@ void MainWindow::on_spinBox_CatchRate_valueChanged(int) {
 void MainWindow::on_spinBox_Base_HP_valueChanged(int) {
     if(not_in_init){
         buf8 = ui->comboBox_PokemonSpecies->currentIndex();
-        pkm_base_hp[buf8] = ui->spinBox_Base_HP->value();
+        pkm_base_hp[buf8+1] = ui->spinBox_Base_HP->value();
     }
 }
 void MainWindow::on_spinBox_Base_Attack_valueChanged(int) {
     if(not_in_init){
         buf8 = ui->comboBox_PokemonSpecies->currentIndex();
-        pkm_base_atk[buf8] = ui->spinBox_Base_Attack->value();
+        pkm_base_atk[buf8+1] = ui->spinBox_Base_Attack->value();
     }
 }
 void MainWindow::on_spinBox_Base_Defense_valueChanged(int) {
     if(not_in_init){
         buf8 = ui->comboBox_PokemonSpecies->currentIndex();
-        pkm_base_def[buf8] = ui->spinBox_Base_Defense->value();
+        pkm_base_def[buf8+1] = ui->spinBox_Base_Defense->value();
     }
 }
 void MainWindow::on_spinBox_Base_Special_valueChanged(int) {
     if(not_in_init){
         buf8 = ui->comboBox_PokemonSpecies->currentIndex();
-        pkm_base_spc[buf8] = ui->spinBox_Base_Special->value();
+        pkm_base_spc[buf8+1] = ui->spinBox_Base_Special->value();
     }
 }
 void MainWindow::on_spinBox_Base_Speed_valueChanged(int) {
     if(not_in_init){
         buf8 = ui->comboBox_PokemonSpecies->currentIndex();
-        pkm_base_speed[buf8] = ui->spinBox_Base_Speed->value();
+        pkm_base_speed[buf8+1] = ui->spinBox_Base_Speed->value();
     }
 }
