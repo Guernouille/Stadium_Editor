ddf61befe59a2713c21302e2339444c122b4129f
Guernouille <froggy25@hotmail.fr>
2016-10-24 04:27:37 +0200

    Edit move data

--- sources/autoconn_edit_move_data.cpp
+++ sources/autoconn_edit_move_data.cpp
@@ -1,7 +1,60 @@
 #include "mainwindow.h"
 
+void MainWindow::on_comboBox_MoveEffect_currentIndexChanged(int) {
+    if(not_in_init){
+        buf8 = ui->comboBox_MovesList->currentIndex();
+        move_effect[buf8] = ui->comboBox_MoveEffect->value();
+    }
+}
+void MainWindow::on_comboBox_MoveType_currentIndexChanged(int) {
+    if(not_in_init){
+        buf8 = ui->comboBox_MovesList->currentIndex();
+        move_type[buf8] = ui->comboBox_MoveType->value();
+    }
+}
+
+
+void MainWindow::on_comboBox_HighCH_MovesList_1_currentIndexChanged(int) {
+    if(not_in_init){
+        move_high_ch[0] = ui->comboBox_HighCH_MovesList_1->value();
+    }
+}
+void MainWindow::on_comboBox_HighCH_MovesList_2_currentIndexChanged(int) {
+    if(not_in_init){
+        move_high_ch[1] = ui->comboBox_HighCH_MovesList_2->value();
+    }
+}
+void MainWindow::on_comboBox_HighCH_MovesList_3_currentIndexChanged(int) {
+    if(not_in_init){
+        move_high_ch[2] = ui->comboBox_HighCH_MovesList_3->value();
+    }
+}
+void MainWindow::on_comboBox_HighCH_MovesList_4_currentIndexChanged(int) {
+    if(not_in_init){
+        move_high_ch[3] = ui->comboBox_HighCH_MovesList_4->value();
+    }
+}
+
+
 void MainWindow::on_spinBox_HighCH_multiplier_valueChanged(int) {
     if(not_in_init){
         move_high_ch_multiplier = ui->spinBox_HighCH_multiplier->value();
+    }
+}
+
+
+void MainWindow::on_spinBox_MovePower_valueChanged(int) {
+    if(not_in_init){
+        move_power = ui->spinBox_MovePower->value();
+    }
+}
+void MainWindow::on_spinBox_MoveAccuracy_valueChanged(int) {
+    if(not_in_init){
+        move_accuracy = ui->spinBox_MoveAccuracy->value();
+    }
+}
+void MainWindow::on_spinBox_MovePP_valueChanged(int) {
+    if(not_in_init){
+        move_pp = ui->spinBox_MovePP->value();
     }
 }
