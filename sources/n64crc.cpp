656fc3aa0b252945f92fae27a3b67c0ecc4206a5
Guernouille <froggy25@hotmail.fr>
2016-10-27 09:06:25 +0200

    create n64crc function to fix CRC

--- sources/n64crc.cpp
+++ sources/n64crc.cpp
@@ -1,41 +1,17 @@
-/* n64crc - N64 cartridge header CRC fix, grabbed on n64dev.org
+/*
+ * N64 cartridge header CRC fix
+ * Adapted from n64crc.c, available on n64dev.org
  *
- * Mar 23, 2010: addition by spinout to actually fix CRC if it is incorrect
- *
- * Copyright notice for this file:
- *  Copyright (C) 2005 Parasyte
- *
+ * Original version is (C) 2005 Parasyte, GNU General Public License
  * Based on uCON64's N64 checksum algorithm by Andreas Sterbenz
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include <stdio.h>
-#include <stdlib.h>
+#include "mainwindow.h"
 
 #define ROL(i, b) (((i) << (b)) | ((i) >> (32 - (b))))
-#define BYTES2LONG(b) ( (b)[0] << 24 | \
-                        (b)[1] << 16 | \
-                        (b)[2] <<  8 | \
-                        (b)[3] )
 
 #define N64_HEADER_SIZE  0x40
 #define N64_BC_SIZE      (0x1000 - N64_HEADER_SIZE)
-
-#define N64_CRC1         0x10
-#define N64_CRC2         0x14
 
 #define CHECKSUM_START   0x00001000
 #define CHECKSUM_LENGTH  0x00100000
@@ -44,63 +20,55 @@
 #define CHECKSUM_CIC6105 0xDF26F436
 #define CHECKSUM_CIC6106 0x1FEA617A
 
-#define Write32(Buffer, Offset, Value)\
-        Buffer[Offset] = (Value & 0xFF000000) >> 24;\
-        Buffer[Offset + 1] = (Value & 0x00FF0000) >> 16;\
-        Buffer[Offset + 2] = (Value & 0x0000FF00) >> 8;\
-        Buffer[Offset + 3] = (Value & 0x000000FF);\
-
 unsigned int crc_table[256];
+unsigned int crc, poly, seed;
+unsigned int t1, t2, t3, t4, t5, t6;
+unsigned int r, d;
+unsigned short cic;
 
-void gen_table() {
-    unsigned int crc, poly;
-    int	i, j;
+void MainWindow::n64crc(QFile &romfile)
+{
+    QDataStream read(&romfile);
 
     poly = 0xEDB88320;
-    for (i = 0; i < 256; i++) {
+
+    for (short i=0; i<256; i++) {
         crc = i;
-        for (j = 8; j > 0; j--) {
+        for (short j=8; j>0; j--) {
             if (crc & 1) crc = (crc >> 1) ^ poly;
             else crc >>= 1;
         }
         crc_table[i] = crc;
     }
-}
 
-unsigned int crc32(unsigned char *data, int len) {
-    unsigned int crc = ~0;
-    int i;
-
-    for (i = 0; i < len; i++) {
-        crc = (crc >> 8) ^ crc_table[(crc ^ data[i]) & 0xFF];
+    crc = ~0;
+    romfile.seek(N64_HEADER_SIZE);
+    for(short i=0; i<N64_BC_SIZE; i++) {
+        read>>buf8;
+        crc = (crc >> 8) ^ crc_table[(crc ^ buf8) & 0xFF];
     }
 
-    return ~crc;
-}
-
-
-int N64GetCIC(unsigned char *data) {
-    switch (crc32(&data[N64_HEADER_SIZE], N64_BC_SIZE)) {
-        case 0x6170A4A1: return 6101;
-        case 0x90BB6CB5: return 6102;
-        case 0x0B050EE0: return 6103;
-        case 0x98BC2C86: return 6105;
-        case 0xACC8580A: return 6106;
+    switch (~crc) {
+        case 0x6170A4A1:
+            cic = 6101;
+            break;
+        case 0x90BB6CB5:
+            cic = 6102;
+            break;
+        case 0x0B050EE0:
+            cic = 6103;
+            break;
+        case 0x98BC2C86:
+            cic = 6105;
+            break;
+        case 0xACC8580A:
+            cic = 6106;
+            break;
+        default:
+            cic = 6105;
     }
 
-    return 6105;
-}
-
-int N64CalcCRC(unsigned int *crc, unsigned char *data) {
-    int bootcode, i;
-    unsigned int seed;
-
-    unsigned int t1, t2, t3;
-    unsigned int t4, t5, t6;
-    unsigned int r, d;
-
-
-    switch ((bootcode = N64GetCIC(data))) {
+    switch (cic) {
         case 6101:
         case 6102:
             seed = CHECKSUM_CIC6102;
@@ -115,14 +83,19 @@ int N64CalcCRC(unsigned int *crc, unsigned char *data) {
             seed = CHECKSUM_CIC6106;
             break;
         default:
-            return 1;
+            seed = 0;
+            QMessageBox messageBox;
+            messageBox.setText("Unable to calculate CRC.");
+            messageBox.setWindowTitle("CRC calculation error");
+            messageBox.exec();
     }
-
     t1 = t2 = t3 = t4 = t5 = t6 = seed;
 
-    i = CHECKSUM_START;
-    while (i < (CHECKSUM_START + CHECKSUM_LENGTH)) {
-        d = BYTES2LONG(&data[i]);
+    short i = CHECKSUM_START;
+
+    while(i < (CHECKSUM_START + CHECKSUM_LENGTH)) {
+        romfile.seek(CHECKSUM_START + i);
+        read>>d;
         if ((t6 + d) < t6) t4++;
         t6 += d;
         t3 ^= d;
@@ -131,98 +104,25 @@ int N64CalcCRC(unsigned int *crc, unsigned char *data) {
         if (t2 > d) t2 ^= r;
         else t2 ^= t6 ^ d;
 
-        if (bootcode == 6105) t1 += BYTES2LONG(&data[N64_HEADER_SIZE + 0x0710 + (i & 0xFF)]) ^ d;
+        if (cic == 6105) {
+            romfile.seek(N64_HEADER_SIZE + 0x710 + (i & 0xFF));
+            read>>buf32;
+            t1 += buf32 ^ d;
+        }
         else t1 += t5 ^ d;
 
         i += 4;
     }
-    if (bootcode == 6103) {
-        crc[0] = (t6 ^ t4) + t3;
-        crc[1] = (t5 ^ t2) + t1;
+    if (cic == 6103) {
+        header_crc1 = (t6 ^ t4) + t3;
+        header_crc2 = (t5 ^ t2) + t1;
     }
-    else if (bootcode == 6106) {
-        crc[0] = (t6 * t4) + t3;
-        crc[1] = (t5 * t2) + t1;
-    }
-    else {
-        crc[0] = t6 ^ t4 ^ t3;
-        crc[1] = t5 ^ t2 ^ t1;
-    }
-
-    return 0;
-}
-
-int main(int argc, char **argv) {
-    FILE *fin;
-    int cic;
-    unsigned int crc[2];
-    unsigned char *buffer;
-
-    //Init CRC algorithm
-    gen_table();
-
-    //Check args
-    if (argc != 2) {
-        printf("Usage: n64sums <infile>\n");
-        return 1;
-    }
-
-    //Open file
-    if (!(fin = fopen(argv[1], "r+b"))) {
-        printf("Unable to open \"%s\" in mode \"%s\"\n", argv[1], "r+b");
-        return 1;
-    }
-
-    //Allocate memory
-    if (!(buffer = (unsigned char*)malloc((CHECKSUM_START + CHECKSUM_LENGTH)))) {
-        printf("Unable to allocate %d bytes of memory\n", (CHECKSUM_START + CHECKSUM_LENGTH));
-        fclose(fin);
-        return 1;
-    }
-
-    //Read data
-    if (fread(buffer, 1, (CHECKSUM_START + CHECKSUM_LENGTH), fin) != (CHECKSUM_START + CHECKSUM_LENGTH)) {
-        printf("Unable to read %d bytes of data (invalid N64 image?)\n", (CHECKSUM_START + CHECKSUM_LENGTH));
-        fclose(fin);
-        free(buffer);
-        return 1;
-    }
-
-    //Check CIC BootChip
-    cic = N64GetCIC(buffer);
-    printf("BootChip: ");
-    printf((cic ? "CIC-NUS-%d\n" : "Unknown\n"), cic);
-
-    //Calculate CRC
-    if (N64CalcCRC(crc, buffer)) {
-        printf("Unable to calculate CRC\n");
+    else if (cic == 6106) {
+        header_crc1 = (t6 * t4) + t3;
+        header_crc2 = (t5 * t2) + t1;
     }
     else {
-        printf("CRC 1: 0x%08X  ", BYTES2LONG(&buffer[N64_CRC1]));
-        printf("Calculated: 0x%08X ", crc[0]);
-        if (crc[0] == BYTES2LONG(&buffer[N64_CRC1]))
-            printf("(Good)\n");
-        else{
-            Write32(buffer, N64_CRC1, crc[0]);
-            fseek(fin, N64_CRC1, SEEK_SET);
-            fwrite(&buffer[N64_CRC1], 1, 4, fin);
-            printf("(Bad, fixed)\n");
-        }
-
-        printf("CRC 2: 0x%08X  ", BYTES2LONG(&buffer[N64_CRC2]));
-        printf("Calculated: 0x%08X ", crc[1]);
-        if (crc[1] == BYTES2LONG(&buffer[N64_CRC2]))
-            printf("(Good)\n");
-        else{
-            Write32(buffer, N64_CRC2, crc[1]);
-            fseek(fin, N64_CRC2, SEEK_SET);
-            fwrite(&buffer[N64_CRC2], 1, 4, fin);
-            printf("(Bad, fixed)\n");
-        }
+        header_crc1 = t6 ^ t4 ^ t3;
+        header_crc2 = t5 ^ t2 ^ t1;
     }
-
-    fclose(fin);
-    free(buffer);
-
-    return 0;
 }
