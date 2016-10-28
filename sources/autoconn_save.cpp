d0acfc8220957c2c3a6f4aef5aebbddedbb0e870
Guernouille <froggy25@hotmail.fr>
2016-10-28 07:21:53 +0200

    brbrbrbrbrbrbrbrbr

--- sources/autoconn_save.cpp
+++ sources/autoconn_save.cpp
@@ -4,12 +4,12 @@ void MainWindow::on_actionSave_triggered(){
     QFile romfile(filename);
     romfile.open(QIODevice::ReadWrite);
 
-    write_n64crc(romfile);
     write_move_data(romfile);
     write_pkm_data(romfile);
     write_cpu_rentals(romfile);
     write_type_chart(romfile);
     n64crc(romfile);
+    write_n64crc(romfile);
 
     romfile.close();
 }
