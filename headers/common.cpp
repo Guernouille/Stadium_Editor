22625e5df918daefdda7d7f013c1353b27b01b36
Guernouille <froggy25@hotmail.fr>
2016-11-24 09:51:11 +0100

    Base Stats constants

--- headers/common.cpp
+++ headers/common.cpp
@@ -4,5 +4,6 @@
 #include <QtCore>
 
 enum romType{INVALID = 0, JAPANESE = 1, US_ENGLISH = 2, EU_ENGLISH = 3, FRENCH = 4, GERMAN = 5, SPANISH = 6};
+enum statType{STAT_HP = 0, STAT_ATK = 1, STAT_DEF = 2, STAT_SPC = 3, STAT_SPEED = 4};
 
 #endif //COMMON
