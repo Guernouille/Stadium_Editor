92bb8bef5d4b85ee6cacdcd6efc829eb2912aa43
Guernouille <froggy25@hotmail.fr>
2016-11-27 03:40:36 +0100

    Refresh TM/HM moves

--- sources/display_move_data.cpp
+++ sources/display_move_data.cpp
@@ -1,7 +1,7 @@
 #include "mainwindow.h"
 
-void MainWindow::display_move_data(quint8 move_id) {
-    not_in_init=false;
+void MainWindow::display_move_data(quint8 move_id)
+{
     ui->lineEdit_Move_Name->setText(move_name[move_id]);
     ui->spinBox_MoveName_Pointer->setValue(move_name_pointer[move_id]);
     ui->spinBox_MoveID->setValue(move_iid[move_id]);
@@ -11,5 +11,23 @@ void MainWindow::display_move_data(quint8 move_id) {
     ui->comboBox_MoveType->setCurrentIndex(move_type[move_id]);
     ui->comboBox_MoveEffect->setCurrentIndex(move_effect[move_id]);
     ui->textEdit_MoveDescrText->setText(move_description[move_id]);
-    not_in_init=true;
+}
+
+void MainWindow::display_tmhm_data()
+{
+    for(short i=1;i<56;i++){
+        ui->tableWidget_MoveTM->setItem(i-1,1,new QTableWidgetItem(move_name[move_tmhm[i]]));
+
+        move_tmhm_flag = (i-1)%8;
+        move_tmhm_byte = (i-1)/8;
+
+        QCheckBox *move_tmhm_chkbox = qobject_cast<QCheckBox *>(ui->tableWidget_MoveTM->cellWidget(i-1,0));
+
+        if((1<<move_tmhm_flag) & pkm_tmhm_flags[1][move_tmhm_byte]){
+            move_tmhm_chkbox->setChecked(true);
+        }
+        else{
+            move_tmhm_chkbox->setChecked(false);
+        }
+    }
 }
