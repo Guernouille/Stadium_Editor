87ca8b8e708fbc92072d6f5f67b6c9ca462be092
Guernouille <froggy25@hotmail.fr>
2016-11-29 07:52:16 +0100

    Release 0.2

--- sources/randomize_move_data.cpp
+++ sources/randomize_move_data.cpp
@@ -11,10 +11,10 @@ void MainWindow::randomize_moves_accuracy(std::mt19937 &mt_rand)
     std::uniform_int_distribution<> rand_accuracy_3(216,240);
     std::uniform_int_distribution<> rand_accuracy_4(200,230);
     std::uniform_int_distribution<> rand_accuracy_5(179,216);
-    std::uniform_int_distribution<> rand_accuracy_6(110,150);
+    std::uniform_int_distribution<> rand_accuracy_6(111,150);
     std::uniform_int_distribution<> rand_accuracy_default(205,255);
     std::uniform_int_distribution<> rand_accuracy_sleep(150,200);
-    std::uniform_int_distribution<> rand_accuracy_wrap(183,222);
+    std::uniform_int_distribution<> rand_accuracy_wrap(187,222);
 
     for(uint8_t i=1; i<total_move_name; i++) {
         // OHKO
@@ -60,29 +60,14 @@ void MainWindow::randomize_moves_accuracy(std::mt19937 &mt_rand)
                     move_accuracy[i] = 255;
                     move_effect[i] = 0x11;
                 }
-                else if(move_power[i] <= 50) {
-                    move_accuracy[i] = 255;
-                    move_effect[i] = 0;
-                }
-                else if(move_power[i] <= 60) {
-                    move_accuracy[i] = rand_accuracy_1(mt_rand);
-                    move_effect[i] = 0;
-                }
-                else if(move_power[i] <= 70) {
-                    move_accuracy[i] = rand_accuracy_2(mt_rand);
-                    move_effect[i] = 0;
-                }
-                else if(move_power[i] <= 80) {
-                    move_accuracy[i] = rand_accuracy_3(mt_rand);
-                    move_effect[i] = 0;
-                }
-                else if(move_power[i] <= 90) {
-                    move_accuracy[i] = rand_accuracy_4(mt_rand);
-                    move_effect[i] = 0;
-                }
                 else {
-                    move_accuracy[i] = rand_accuracy_6(mt_rand);
                     move_effect[i] = 0;
+                    if(move_power[i] <= 50) move_accuracy[i] = 255;
+                    else if(move_power[i] <= 60) move_accuracy[i] = rand_accuracy_1(mt_rand);
+                    else if(move_power[i] <= 70) move_accuracy[i] = rand_accuracy_2(mt_rand);
+                    else if(move_power[i] <= 80) move_accuracy[i] = rand_accuracy_3(mt_rand);
+                    else if(move_power[i] <= 90) move_accuracy[i] = rand_accuracy_wrap(mt_rand);
+                    else move_accuracy[i] = rand_accuracy_6(mt_rand);
                 }
             }
 
@@ -93,14 +78,18 @@ void MainWindow::randomize_moves_accuracy(std::mt19937 &mt_rand)
                     move_accuracy[i] = 255;
                     move_effect[i] = 0x11;
                 }
-                else if(move_power[i] <= 80) move_accuracy[i] = 255;
-                else if(move_power[i] <= 95) move_accuracy[i] = rand_accuracy_1(mt_rand);
-                else if(move_power[i] <= 100) move_accuracy[i] = rand_accuracy_2(mt_rand);
-                else if(move_power[i] <= 110) move_accuracy[i] = rand_accuracy_3(mt_rand);
-                else if(move_power[i] <= 120) move_accuracy[i] = rand_accuracy_4(mt_rand);
-                else if(move_power[i] <= 130) move_accuracy[i] = rand_accuracy_5(mt_rand);
-                else move_accuracy[i] = rand_accuracy_6(mt_rand);
+                else {
+                    move_effect[i] = 0;
+                    if(move_power[i] <= 80) move_accuracy[i] = 255;
+                    else if(move_power[i] <= 95) move_accuracy[i] = rand_accuracy_1(mt_rand);
+                    else if(move_power[i] <= 100) move_accuracy[i] = rand_accuracy_2(mt_rand);
+                    else if(move_power[i] <= 110) move_accuracy[i] = rand_accuracy_3(mt_rand);
+                    else if(move_power[i] <= 120) move_accuracy[i] = rand_accuracy_4(mt_rand);
+                    else if(move_power[i] <= 130) move_accuracy[i] = rand_accuracy_5(mt_rand);
+                    else move_accuracy[i] = rand_accuracy_6(mt_rand);
+                }
             }
+
             // Recoil moves
             else if(move_effect[i] == 0x30) {
                 if(move_power[i] <= 95) move_accuracy[i] = 255;
@@ -109,6 +98,7 @@ void MainWindow::randomize_moves_accuracy(std::mt19937 &mt_rand)
                 else if(move_power[i] <= 130) move_accuracy[i] = rand_accuracy_4(mt_rand);
                 else move_accuracy[i] = rand_accuracy_5(mt_rand);
             }
+
             // Thrash
             else if(move_effect[i] == 0x1B) {
                 if(move_power[i] <= 100) move_accuracy[i] = 255;
@@ -117,12 +107,14 @@ void MainWindow::randomize_moves_accuracy(std::mt19937 &mt_rand)
                 else if(move_power[i] <= 130) move_accuracy[i] = rand_accuracy_4(mt_rand);
                 else move_accuracy[i] = rand_accuracy_5(mt_rand);
             }
+
             // Two-turn moves
             else if(move_effect[i] == 0x27) {
                 if(move_power[i] <= 135) move_accuracy[i] = 255;
                 else if(move_power[i] <= 175) move_accuracy[i] = rand_accuracy_1(mt_rand);
                 else move_accuracy[i] = rand_accuracy_2(mt_rand);
             }
+
             // Recharge moves
             else if(move_effect[i] == 0x50) {
                 if(move_power[i] <= 135) move_accuracy[i] = rand_accuracy_1(mt_rand);
@@ -130,6 +122,7 @@ void MainWindow::randomize_moves_accuracy(std::mt19937 &mt_rand)
                 else if(move_power[i] <= 170) move_accuracy[i] = rand_accuracy_3(mt_rand);
                 else move_accuracy[i] = rand_accuracy_4(mt_rand);
             }
+
             // Multi-hit moves
             else if(move_effect[i] == 0x1D) {
                 if(move_power[i] <= 21) move_accuracy[i] = 255;
@@ -140,6 +133,7 @@ void MainWindow::randomize_moves_accuracy(std::mt19937 &mt_rand)
                 else if(move_power[i] <= 33) move_accuracy[i] = rand_accuracy_5(mt_rand);
                 else move_accuracy[i] = rand_accuracy_6(mt_rand);
             }
+
             // Two-hit moves
             else if(move_effect[i] == 0x2C || move_effect[i] == 0x4D) {
                 if(move_power[i] <= 35) move_accuracy[i] = 255;
@@ -150,15 +144,19 @@ void MainWindow::randomize_moves_accuracy(std::mt19937 &mt_rand)
                 else if(move_power[i] <= 65) move_accuracy[i] = rand_accuracy_5(mt_rand);
                 else move_accuracy[i] = rand_accuracy_6(mt_rand);
             }
-            // Good added effects
-            else if(move_effect[i] == 0x03 || move_effect[i] == 0x25) {
-                if(move_power[i] <= 78) move_accuracy[i] = 255;
-                else if(move_power[i] <= 91) move_accuracy[i] = rand_accuracy_1(mt_rand);
-                else if(move_power[i] <= 100) move_accuracy[i] = rand_accuracy_3(mt_rand);
-                else if(move_power[i] <= 109) move_accuracy[i] = rand_accuracy_4(mt_rand);
-                else if(move_power[i] <= 118) move_accuracy[i] = rand_accuracy_5(mt_rand);
+
+            // 10% Burn / Paralysis / Freeze
+            else if((move_effect[i] >= 0x04 && move_effect[i] <= 0x06) || move_effect[i] == 0x23) {
+                if(move_power[i] <= 75) move_accuracy[i] = 255;
+                else if(move_power[i] <= 85) move_accuracy[i] = rand_accuracy_1(mt_rand);
+                else if(move_power[i] <= 95) move_accuracy[i] = rand_accuracy_2(mt_rand);
+                else if(move_power[i] <= 105) move_accuracy[i] = rand_accuracy_3(mt_rand);
+                else if(move_power[i] <= 115) move_accuracy[i] = rand_accuracy_4(mt_rand);
+                else if(move_power[i] <= 125) move_accuracy[i] = rand_accuracy_5(mt_rand);
                 else move_accuracy[i] = rand_accuracy_6(mt_rand);
             }
+
+            // 30% Burn, 30% Paralysis, 30% Stat -1 (bar Evasion)
             else if(move_effect[i] == 0x22 || move_effect[i] == 0x24 || (move_effect[i] >= 0x44 && move_effect[i] <= 0x48)) {
                 if(move_power[i] <= 60) move_accuracy[i] = 255;
                 else if(move_power[i] <= 80) move_accuracy[i] = rand_accuracy_1(mt_rand);
@@ -168,14 +166,34 @@ void MainWindow::randomize_moves_accuracy(std::mt19937 &mt_rand)
                 else if(move_power[i] <= 108) move_accuracy[i] = rand_accuracy_5(mt_rand);
                 else move_accuracy[i] = rand_accuracy_6(mt_rand);
             }
-            // 10% Burn / Paralysis / Freeze
-            else if((move_effect[i] >= 0x04 && move_effect[i] <= 0x06) || move_effect[i] == 0x23) {
+
+            // 30% Flinch
+            else if(move_effect[i] == 0x03 || move_effect[i] == 0x25) {
+                if(move_power[i] <= 69) move_accuracy[i] = 255;
+                else if(move_power[i] <= 80) move_accuracy[i] = rand_accuracy_1(mt_rand);
+                else if(move_power[i] <= 90) move_accuracy[i] = rand_accuracy_3(mt_rand);
+                else if(move_power[i] <= 100) move_accuracy[i] = rand_accuracy_4(mt_rand);
+                else if(move_power[i] <= 110) move_accuracy[i] = rand_accuracy_5(mt_rand);
+                else move_accuracy[i] = rand_accuracy_6(mt_rand);
+            }
+
+            // Mega Drain
+            else if(move_effect[i] == 0x03 || move_effect[i] == 0x25) {
                 if(move_power[i] <= 75) move_accuracy[i] = 255;
-                else if(move_power[i] <= 85) move_accuracy[i] = rand_accuracy_1(mt_rand);
-                else if(move_power[i] <= 95) move_accuracy[i] = rand_accuracy_2(mt_rand);
-                else if(move_power[i] <= 105) move_accuracy[i] = rand_accuracy_3(mt_rand);
-                else if(move_power[i] <= 115) move_accuracy[i] = rand_accuracy_4(mt_rand);
-                else if(move_power[i] <= 125) move_accuracy[i] = rand_accuracy_5(mt_rand);
+                else if(move_power[i] <= 88) move_accuracy[i] = rand_accuracy_1(mt_rand);
+                else if(move_power[i] <= 97) move_accuracy[i] = rand_accuracy_3(mt_rand);
+                else if(move_power[i] <= 109) move_accuracy[i] = rand_accuracy_4(mt_rand);
+                else if(move_power[i] <= 118) move_accuracy[i] = rand_accuracy_5(mt_rand);
+                else move_accuracy[i] = rand_accuracy_6(mt_rand);
+            }
+
+            // Other damaging moves
+            else if(move_power[i] > 1) {
+                if(move_power[i] <= 75) move_accuracy[i] = 255;
+                else if(move_power[i] <= 95) move_accuracy[i] = rand_accuracy_1(mt_rand);
+                else if(move_power[i] <= 102) move_accuracy[i] = rand_accuracy_3(mt_rand);
+                else if(move_power[i] <= 110) move_accuracy[i] = rand_accuracy_4(mt_rand);
+                else if(move_power[i] <= 120) move_accuracy[i] = rand_accuracy_5(mt_rand);
                 else move_accuracy[i] = rand_accuracy_6(mt_rand);
             }
 
@@ -201,6 +219,8 @@ void MainWindow::randomize_moves_accuracy(std::mt19937 &mt_rand)
 void MainWindow::randomize_moves_effects(std::mt19937 &mt_rand)
 {
     bool no_explosion_change = ui->checkBox_Randomizer_MoveData_Explosion->isChecked();
+    bool no_useless_moves_cpu = ui->checkBox_Randomizer_CPU_NoUselessMoves->isChecked();
+    bool no_useless_moves_rental = ui->checkBox_Randomizer_Rental_NoUselessMoves->isChecked();
 
     std::array<int,36> effects_nodmg {0x9,0xA,0xB,0xC,0xD,0xE,0xF,0x12,0x13,0x14,0x16,0x17,0x18,0x19,0x20,0x2E,0x2F,0x31,0x32,0x33,0x34,0x35,0x39,0x3A,0x3B,0x3C,0x3F,0x40,0x41,0x43,0x4F,0x52,0x53,0x54,0x55,0x56};
     std::array<int,30> effects_dmg {0x2,0x3,0x4,0x5,0x6,0x7,0x8,0x11,0x1B,0x1D,0x1F,0x21,0x22,0x23,0x24,0x25,0x27,0x2A,0x2B,0x2C,0x2D,0x30,0x44,0x45,0x46,0x47,0x49,0x4C,0x4D,0x50};
@@ -212,7 +232,7 @@ void MainWindow::randomize_moves_effects(std::mt19937 &mt_rand)
         /* Exclude Counter, Metronome, Quick Attack, Toxic, Transform and Fixed damage moves.
          * Exclude Explosion and Selfdestruct if the option is checked
          */
-        if((no_explosion_change == false || move_effect[i] != 7) && move_effect[i] != 0x28 && move_effect[i] != 0x29 && move_effect[i] != 0x39 && move_effect[i] != 0x53 &&
+        if((no_explosion_change==false || move_effect[i] != 0x7 || (i != 0x78 && i != 0x99)) && move_effect[i] != 0x28 && move_effect[i] != 0x29 && move_effect[i] != 0x39 && move_effect[i] != 0x53 &&
             i != 0x44 && i != 0x5C && i != 0x62)
         {
             // Non-damaging moves
@@ -221,12 +241,22 @@ void MainWindow::randomize_moves_effects(std::mt19937 &mt_rand)
 
                 move_effect[i] = effects_nodmg[0];
                 // 0x35: Special +2
-                if(move_effect[i] == 0x35 && (buf8 & 1) == 1) move_effect[i] = effects_nodmg[1];
-                else buf8 &= 1;
+                if(move_effect[i] == 0x35) {
+                    if((buf8 & 1) == 1) move_effect[i] = effects_nodmg[1];
+                    else buf8 &= 1;
+                }
 
                 // 0x38: Recover (except Rest)
-                if(move_effect[i] == 0x38 && (buf8 & 2) == 2 && i != 0x9C) move_effect[i] = effects_nodmg[2];
-                else if(i !=0x9C) buf8 &= 2;
+                if(move_effect[i] == 0x38) {
+                    if((buf8 & 2) == 2 && i != 0x9C) move_effect[i] = effects_nodmg[2];
+                    else if(i !=0x9C) buf8 &= 2;
+                }
+
+                // 0x55: Splash
+                if(move_effect[i] == 0x55) {
+                    if((no_useless_moves_cpu && no_useless_moves_rental) || (buf8 & 4) == 4) move_effect[i] = effects_nodmg[3];
+                    else buf8 &= 4;
+                }
             }
             // Damaging moves
             else if(move_power[i] > 1){
@@ -238,8 +268,10 @@ void MainWindow::randomize_moves_effects(std::mt19937 &mt_rand)
                     move_effect[i] = effects_dmg[0];
 
                     // 0x08: Dream Eater
-                    if(move_effect[i] == 0x08 && (buf8 & 4) == 4) move_effect[i] = effects_dmg[1];
-                    else buf8 &= 4;
+                    if(move_effect[i] == 0x08) {
+                        if((buf8 & 8) == 8) move_effect[i] = effects_dmg[1];
+                        else buf8 &= 8;
+                    }
 
                     // 0x26: OHKO
                     if(move_effect[i] == 0x26) {
@@ -248,8 +280,10 @@ void MainWindow::randomize_moves_effects(std::mt19937 &mt_rand)
                     }
 
                     // 0x47: 30% Special -1
-                    if(move_effect[i] == 0x47 && (buf8 & 8) == 8) move_effect[i] = effects_dmg[1];
-                    else buf8 &= 8;
+                    if(move_effect[i] == 0x47) {
+                        if((buf8 & 16) == 16) move_effect[i] = effects_dmg[1];
+                        else buf8 &= 16;
+                    }
                 }
             }
         }
@@ -311,17 +345,17 @@ void MainWindow::randomize_moves_power(std::mt19937 &mt_rand)
     buf8 = buf16/2;
     buf16 = buf8 + moverand_max;
     buf8 = buf16/2;
-    std::uniform_int_distribution<> rand_power_high_ch(buf8,moverand_max);
+    std::uniform_int_distribution<> rand_power_dream_eater(buf8,moverand_max);
 
     // High CH moves + Quick Attack
     buf8 = moverand_max/2;
-    std::uniform_int_distribution<> rand_power_dream_eater(moverand_min,buf8);
+    std::uniform_int_distribution<> rand_power_high_ch(moverand_min,buf8);
 
 
     // Randomize Base Powers
     for(uint8_t i=1; i<total_move_name; i++) {
         // Exclude Explosion and Selfdestruct if the option is checked
-        if(move_power[i] > 1 && (no_explosion_change==false || move_effect[i]!=7)) {
+        if(move_power[i] > 1 && (no_explosion_change==false || move_effect[i] != 0x7 || (i != 0x78 && i != 0x99))) {
             // High CH moves + Quick Attack
             if(i == move_high_ch[0] || i == move_high_ch[1] || i == move_high_ch[2] || i == move_high_ch[3] || i == 0x62)
                 move_power[i] = rand_power_high_ch(mt_rand);
@@ -331,7 +365,7 @@ void MainWindow::randomize_moves_power(std::mt19937 &mt_rand)
                 move_power[i] = rand_power_no_effect(mt_rand);
 
             // Dream Eater
-            else if(move_effect[i] == 0x08)
+            else if(move_effect[i] == 0x8)
                 move_power[i] = rand_power_dream_eater(mt_rand);
 
             // Recoil moves, Hi Jump Kick, Thrash
@@ -342,8 +376,8 @@ void MainWindow::randomize_moves_power(std::mt19937 &mt_rand)
             else if(move_effect[i] == 0x50)
                 move_power[i] = rand_power_recharge(mt_rand);
 
-            // Two-turn moves
-            else if(move_effect[i] == 0x27)
+            // Explosion, Two-turn moves
+            else if(move_effect[i] == 0x7 || move_effect[i] == 0x27)
                 move_power[i] = rand_power_twoturn(mt_rand);
 
             // Swift
@@ -377,7 +411,7 @@ void MainWindow::randomize_moves_pp(std::mt19937 &mt_rand) {
 
     for(uint8_t i=1; i<total_move_name; i++) {
         // Exclude Explosion and Selfdestruct if the option is checked, and Fixed damage moves
-        if((no_explosion_change==false || move_effect[i]!=7) && move_effect[i] != 0x28 && move_effect[i] != 0x29) {
+        if((no_explosion_change==false || move_effect[i] != 0x7 || (i != 0x78 && i != 0x99)) && move_effect[i] != 0x28 && move_effect[i] != 0x29) {
             // Explosion, OHKO, Evasion -2
             if(move_effect[i] == 0x7 || move_effect[i] == 0x26 || move_effect[i] == 0x3F)
                 move_pp[i] = 5;
@@ -606,8 +640,8 @@ void MainWindow::randomize_moves_types(std::mt19937 &mt_rand)
          * Exclude OHKO as their Type is defined in randomize_moves_effects
          * Exclude Explosion and Selfdestruct if the option is checked
          */
-        if((no_explosion_change == false || move_effect[i] != 7) && move_effect[i] != 0x26 && move_effect[i] != 0x28 && move_effect[i] != 0x29 && move_effect[i] != 0x39 &&
-            move_effect[i] != 0x53 && i != 0x44 && i != 0x5C)
+        if((no_explosion_change==false || move_effect[i] != 0x7 || (i != 0x78 && i != 0x99)) && move_effect[i] != 0x26 && move_effect[i] != 0x28 &&
+           move_effect[i] != 0x29 && move_effect[i] != 0x39 && move_effect[i] != 0x53 && i != 0x44 && i != 0x5C)
         {
             move_type[i] = mt_rand()%16;
             if(move_type[i] == 6) move_type[i] = 0;
